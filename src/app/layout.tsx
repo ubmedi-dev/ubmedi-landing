2d0a81d062f562a319fa8045b9bb2431115e42e1
kjw <kjwon8202002@gmail.com>
2026-07-02 11:22:06 +0900

    feat: 네이버 메타데이터 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "UB MEDI | 프리미엄 맞춤 메디컬 투어",
   description: "한국 프리미엄 메디컬 투어를 위한 UB MEDI 랜딩 페이지",
+  verification: {
+    other: {
+      "naver-site-verification": "a6be6a9d3401c79a23041b3fa795b4aa2769556b",
+    },
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
